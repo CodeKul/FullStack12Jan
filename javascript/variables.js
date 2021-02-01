5d6fc58db2d765aa749606663ac412fa17a140b9
sucheta-codekul <sucheta.codekul@gmail.com>
2021-02-01 17:36:07 +0530

    media-queries,js variables

---
+++ javascript/variables.js
@@ -0,0 +1,25 @@
+var x; //declaring
+var x = 10; //initialization
+x = 20; //assignment
+
+var result; //declaring result
+var result = 100; //initialization
+
+result = 500; //assignment
+
+var a = 100;
+var a = 300;
+a = 500;
+
+console.log(a);
+document.write(result);
+
+//let ,const
+
+let number = 100;
+ number = 200;
+console.log(number);
+
+const pi = 100; 
+
+console.log(pi);
